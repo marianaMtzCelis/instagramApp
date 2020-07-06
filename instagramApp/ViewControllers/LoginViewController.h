9c6124da8090a2324b70477b276a73050f5cda54
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-06 18:02:17 -0500

    User can sign up a new account

---
+++ instagramApp/ViewControllers/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  instagramApp
+//
+//  Created by Mariana Martinez on 06/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
