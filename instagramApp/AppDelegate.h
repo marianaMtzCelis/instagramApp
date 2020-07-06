fa712aa78b559ea5b52d16df9ec6eaf68716d732
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-06 16:49:25 -0500

    Initial Commit

---
+++ instagramApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  instagramApp
+//
+//  Created by Mariana Martinez on 06/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ instagramApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  instagramApp
+//
+//  Created by Mariana Martinez on 06/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
