274dd276e3482f75019b60701927584658912e5f
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-08 12:23:18 -0500

    user can make a post and is saved on the parse data base

---
+++ instagramApp/Models/Post.h
@@ -0,0 +1,29 @@
+//
+//  Post.h
+//  instagramApp
+//
+//  Created by Mariana Martinez on 07/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Post : PFObject<PFSubclassing>
+
+@property (nonatomic, strong) NSString *postID;
+@property (nonatomic, strong) NSString *userID;
+@property (nonatomic, strong) PFUser *author;
+
+@property (nonatomic, strong) NSString *caption;
+@property (nonatomic, strong) PFFileObject *image;
+@property (nonatomic, strong) NSNumber *likeCount;
+@property (nonatomic, strong) NSNumber *commentCount;
+
++ (void) postUserImage: ( UIImage * _Nullable )image withCaption: ( NSString * _Nullable )caption withCompletion: (PFBooleanResultBlock  _Nullable)completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
